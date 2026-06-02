134e4e15c8e2d436768cba6bc8d3351b1f279edf
ihirwe-promis <p.ihirwe@alustudent.com>
2026-06-02 07:58:58 -0700

    redo

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -1,1 +1,16 @@
-#!/bin/bash
+!#/bin/bash
+
+secure_data() {
+
+echo "securing medical log data"
+
+if [ -d "active_logs" ]; then
+	chmod 700 active_logs
+
+	echo "permissions have been updated successfully"
+	echo "Updated Permission: "
+	ls -ld active_logs
+else
+	echo "ERROR: active_logs directory is not found here"
+fi
+}
