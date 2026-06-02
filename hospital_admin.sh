21b95dd841e787bfa1c9d824bea3197daf9dea90
nissi15 <ishemanissi228@gmail.com>
2026-06-02 15:57:16 +0200

    Added active_logs and archived_logs

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+# Creating the initialise_system ()
+
+initialise_system () {
+
+# -- Checking and creating acitve_logs 	
+	if [ -d "active_logs" ]; then
+		echo "active_logs already exists"
+
+	else "Creating active_logs directory --------"
+		mkdir active_logs
+
+# -- Checking and creating archived_logs
+	if [ -d "archived_logs" ]; then
+		echo "archived_logs already exists"
+
+	else "Creating archived_logs direcotry ------"
+		mkdir archived_logs
+
+
+
+
+	
